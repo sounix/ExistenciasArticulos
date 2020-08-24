56211c64f2dcb54b0375296ac2f75d23783d9851
Jose Alejandro Lopez Flores <aleflo_1996@outlook.com>
2020-08-24 17:34:16 -0500

    ADD: Agrega details a la interfaz

--- js/index.js
+++ js/index.js
@@ -8,7 +8,9 @@ var app = new Vue({
             {Articulo: "0176001", CodigoBarras: "7501379119056", Nombre: "Azucar Bueno 1kg", Relacion: "1/BTO - 10/PZA", Subfamilia: "76", DescSubfamila: "Azucares A Granel Y Envasados"},
             {Articulo: "0176001", CodigoBarras: "7501379119056", Nombre: "Azucar Bueno 1kg", Relacion: "1/BTO - 10/PZA", Subfamilia: "76", DescSubfamila: "Azucares A Granel Y Envasados"}
         ],
-        movil: false
+        movil: false,
+        articulo: {},
+        showDetails: false
     },
     mounted: function(){
         this.movil = this.isDiplayMovil();
@@ -17,6 +19,14 @@ var app = new Vue({
         isDiplayMovil: function() {
             if (screen.width > 800) return false;
             return true;
+        },
+        setUri: function(uri) {
+            this.showDetails = true;
+            console.log("uri",uri);
+            localStorage.setItem("uri", uri)
+        },
+        backMain: function() {
+            this.showDetails = false;
         }
     }
 });
