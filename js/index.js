b8e6180ad022cd278f92190338ac30a4874957da
Jose Alejandro Lopez Flores <aleflo_1996@outlook.com>
2020-10-09 19:01:16 -0500

    REFACTOR: Mejora visualizacion de valores

--- js/index.js
+++ js/index.js
@@ -26,35 +26,6 @@ var app = new Vue({
             this.messageAlert = message;
             document.getElementById("btnAlert").click();
         },
-        roundTo: function(number, digits = 2, autoComplete = false) { //12411.95368 , 3, false
-            const stringNumber = (number === null || number === undefined) ? "0" : number.toString();
-            const arrayDivision = stringNumber.split(".");
-            const lengthDivision = arrayDivision.length;
-            let rounded = "0.00";
-            let digitsString = "";
-            if (lengthDivision === 1) {
-                if (!autoComplete) return arrayDivision[0];
-
-                for (let index = 0; index < digits; index++) {
-                    digitsString += "0"
-                }
-                rounded = arrayDivision[0] + "." +digitsString;
-                return rounded;
-            }
-            if (digits === 0) {
-                rounded = Math.round(number);
-                return rounded;
-            }
-            digitsString = arrayDivision[1].slice(0, (digits - 1));
-            let digitToRound = parseInt(arrayDivision[1].slice(digits, (digits + 1)));
-            if (isNaN(digitToRound)) digitToRound = 0;
-            let digitRounded = -1;
-            if (digitToRound < 5) digitRounded = arrayDivision[1].slice((digits - 1), digits);
-            if (digitToRound >= 5 && digitToRound < 9) digitRounded = parseInt(arrayDivision[1].slice((digits - 1), digits)) + 1;
-            if (digitToRound == 9) digitRounded = 9;
-            rounded = arrayDivision[0] + "." + digitsString + digitRounded;
-            return rounded;
-        },
         parseToPorcent: function(value) { // 0.45
             const stringValue = value.toString();
             const arrayValue = stringValue.split(".");
