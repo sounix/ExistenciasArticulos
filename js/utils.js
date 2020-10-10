1e13d289dba73c4b03b8b766288e66f2c982db4d
Jose Alejandro Lopez Flores <aleflo_1996@outlook.com>
2020-10-10 11:19:16 -0500

    ADD: Agrega utileria para formatear numeros

--- js/utils.js
+++ js/utils.js
@@ -1,6 +1,5 @@
 var utils = (() => {
     const parseFecha = (stringFecha, withHour = false) => {
-        console.log(stringFecha);
         const fecha = new Date(stringFecha);
         let newFormat = `${stringFecha.slice(8,10)} de ${_arrayMonths[fecha.getMonth()]} del ${fecha.getFullYear()}`;
         if (withHour) {
@@ -9,8 +8,33 @@ var utils = (() => {
         return newFormat
     }
 
-
     const _arrayMonths = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+
+    const aplyFormatNumeric = (numberString) => {
+        if (numberString === null || typeof numberString === "undefined" || numberString.trim().length <= 2) return;
+        const arrayDivision = numberString.split(".");
+        let numberFormated = "00,000.000";
+        const lengthPartInt = arrayDivision[0].length;
+        const sobrantes = ((lengthPartInt)%3);
+        let contadorSecundario = 1;
+        numberFormated = arrayDivision[0].split("").reduce((acumulador, numero, indice) => {
+            acumulador.push(numero);
+            if(indice === (sobrantes-1) && indice < (lengthPartInt-1)) acumulador.push(",");
+            if (indice >= sobrantes && indice < (lengthPartInt-1)) {
+                if ((contadorSecundario)%3 === 0) {
+                    acumulador.push(",");
+                }
+                contadorSecundario++;
+            }
+            return acumulador;
+        },[]);
+        numberFormated = numberFormated.join("");
+        if (arrayDivision.length === 2) {
+            numberFormated += `.${arrayDivision[1]}`;
+        }
+        console.log(numberString, numberFormated);
+        return numberFormated;
+    }
 
     const roundTo = (number, digits = 2, autoComplete = false) => { //12411.95368 , 3, false
         const stringNumber = (number === null || number === undefined) ? "0" : number.toString();
@@ -44,6 +68,7 @@ var utils = (() => {
 
     return {
         parseFecha,
-        roundTo
+        roundTo,
+        aplyFormatNumeric
     }
 })()
